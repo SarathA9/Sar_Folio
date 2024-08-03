78abd692683cbf1cfba8cdd0062b4176066436ed
Sarath <135338134+SarathA9@users.noreply.github.com>
2024-08-03 14:59:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();   
        
 const app = express();   
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
  
 app.use(cors()); // Enable CORS 
    
