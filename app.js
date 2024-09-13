b70d9c1ae7fc9a515807449ac839f43dfb9fa223
Sarath <135338134+SarathA9@users.noreply.github.com>
2024-09-13 21:52:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();   
+require('dotenv').config();     
        
 const app = express();   
 const port = process.env.PORT || 3000; 
