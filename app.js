729b35f0761b13c9c1d23f97c700d2d736497697
Sarath <135338134+SarathA9@users.noreply.github.com>
2024-09-14 20:21:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors()); // Enable CORS
 // Middleware to parse from data
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
-   
+    
 app.use(express.static('public'));
       
 // Handle form submission
